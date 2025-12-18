eb04ac43cfe83c4a40faaeb2dc31e5218d237f02
Dmitry Burchak <burchak.dmitry@icloud.com>
2025-12-18 17:23:31 +0200

    feat(): add logic route pages

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -8,9 +8,11 @@ export const Categories = () => {
     <section className="categories">
       <h2 className="categoires__title main__title">Shop by category</h2>
       <div className="categories__list">
-        {categories.map((category, index) => (
-          <CategoryCard category={category} key={index} />
-        ))}
+        {categories.map((category, index) => {
+          // const lowerCaseCategory = category.toLowerCase();
+
+          return <CategoryCard category={category} key={index} />;
+        })}
       </div>
     </section>
   );
--- src/components/CategoryCard/CategoryCard.tsx
+++ src/components/CategoryCard/CategoryCard.tsx
@@ -1,32 +1,47 @@
 import classNames from 'classnames';
 import './CategoryCard.scss';
+import { useMemo } from 'react';
+import { useAppSelector } from '../../types/hooks';
+import { Link } from 'react-router-dom';
 
 type Props = {
   category: string;
 };
 
 export const CategoryCard: React.FC<Props> = ({ category }) => {
-  const lowerCase = category.toLowerCase();
+  const lowerCaseCategory = category.toLowerCase();
+  const products = useAppSelector(state => state.products.products);
+  const currentCategory = category.toLowerCase();
+  const uniqueModelsMemo = useMemo(
+    () => [...products].filter(product => product.category === currentCategory),
+    [products, currentCategory],
+  );
 
   return (
     <article className="categories__category category">
-      <div
-        className={classNames('category__wrap', `category__wrap--${lowerCase}`)}
+      <Link
+        to={`../${lowerCaseCategory}`}
+        className={classNames(
+          'category__wrap',
+          `category__wrap--${lowerCaseCategory}`,
+        )}
       >
         <div
           className={classNames(
             'category__gadget',
-            `category__gadget--${lowerCase}`,
+            `category__gadget--${lowerCaseCategory}`,
           )}
         ></div>
-      </div>
+      </Link>
       <div className="category__description">
         <h4 className="category__description-title">
           {category !== 'Tablets' && category !== 'Accessories'
             ? 'Mobile phones'
             : category}
         </h4>
-        <p className="category__description-qty">N models</p>
+        <p className="category__description-qty">
+          {uniqueModelsMemo.length} models
+        </p>
       </div>
     </article>
   );
